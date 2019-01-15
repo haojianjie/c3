ba5130d2126fbe21622f2956a80416493c742e78
Your Name <you@example.com>
2019-01-15 23:06:34 +0800

    框架

--- main.c
+++ main.c
@@ -1,9 +1,42 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
+    char arr[100];
+    int index = 0;
+    printf("minganzifuguanlixitong\n");
+    while (1)
+    {
+        int code;
 
-    printf("hello world!");
+        printf("1---zengjiaminganzici\n");
+        printf("2---shanchuzuihouyigeminganci\n");
+        printf("3---xianshiquanbuminganci\n");
+        printf("4---tihuanshuruneirongzhaongdeminganci\n"); //把所有的敏感字符全部替换为*
+        printf("5---tuichuxitong\n");
+
+        printf("qingshuruxiangyingdegongnengbianhao\n");
+
+        scanf("%d", &code);
+
+        if (code == 1)
+        {
+        }
+        if (code == 2)
+        {
+        }
+        if (code == 3)
+        {
+        }
+        if (code == 4)
+        {
+        }
+        if (code == 5)
+        {
+            printf("huiche,tuichuchengxu\n");
+            break;
+        }
+    }
 
     return 0;
-
 }
